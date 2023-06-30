4aae3631fde3e3706ce43e136c4a78ee71174fd4
Maxime Brunet <mbrunet@dawtio.cloud>
2023-06-30 23:27:28 +0200

    feat(homepage): put site under construction

--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+	<p></p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Site under construction ! 🎉
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://dawtio.notion.site"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Explore our Terraform Blog
         </a>
       </header>
     </div>
